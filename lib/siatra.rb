db77d3362c1a42b23b8cbb4c56d8c73518e24406
ujihisa <ujihisa@gmail.com>
2011-03-29 07:20:18 -0700

    adding a command with -h and -v

--- lib/siatra.rb
+++ lib/siatra.rb
@@ -1,3 +1,15 @@
+module Siatra
+  VERSION = '0.1'
+
+  class << self
+    def help
+      puts "    siatra -v          shows version number"
+      puts "    siatra -h          shows this message"
+      puts "    siatra {filename}  shows de-artized content of the file"
+    end
+  end
+end
+
 def Siatra(code)
   code.gsub("\n", ";")
 end
--- spec/siatra_spec.rb
+++ spec/siatra_spec.rb
@@ -1,9 +1,26 @@
 require_relative '../lib/siatra'
+require 'open3'
 
 describe 'Siatra' do
   context '()' do
     it 'converts linefeeds to semicolons' do
       Siatra("this\nis\na\npen.").should == 'this;is;a;pen.'
+    end
+  end
+end
+
+describe '/bin/siatra' do
+  context '-h' do
+    it 'shows help message' do
+      x, _ = Open3.capture2e('./bin/siatra -h')
+      x.should =~ /shows this message/
+    end
+  end
+
+  context '-v' do
+    it 'shows version' do
+      x, _ = Open3.capture2e('./bin/siatra -v')
+      x.should =~ /\d\.\d/
     end
   end
 end
